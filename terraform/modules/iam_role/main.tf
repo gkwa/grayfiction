0ddb281421b085c405cf2949d01332f091f0f621
Taylor Monacelli <taylormonacelli@gmail.com>
2024-04-04 20:03:47 -0700

    failing

--- terraform/modules/iam_role/main.tf
+++ terraform/modules/iam_role/main.tf
@@ -51,3 +51,20 @@ resource "aws_iam_role_policy" "delete_images" {
     ]
   })
 }
+
+resource "aws_iam_role_policy" "describe_regions" {
+  name = "${var.name}-describe-regions"
+  role = aws_iam_role.role.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ec2:DescribeRegions"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
--- terraform/modules/iam_role/variables.tf
+++ terraform/modules/iam_role/variables.tf
@@ -1,4 +1,12 @@
 variable "name" {
-  description = "The name of the IAM role"
-  type        = string
+ description = "The name of the IAM role"
+ type        = string
+}
+
+-- terraform/modules/iam_user/main.tf --
+resource "aws_iam_user" "user" {
+ name = var.name
+ tags = {
+   Name = var.name
+ }
 }
