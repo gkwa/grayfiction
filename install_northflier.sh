f069923e1f7f28574fae8ce5479e1e072435b89e
Taylor Monacelli <taylormonacelli@gmail.com>
2024-04-22 14:33:26 -0400

    Pin ringgem version to track ci builds

--- install_northflier.sh
+++ install_northflier.sh
@@ -4,15 +4,16 @@ set -e
 set -x
 set -u
 
-
 sudo apt-get update
-DEBIAN_FRONTEND=noninteractive sudo apt-get install --assume-yes unzip curl
+DEBIAN_FRONTEND=noninteractive sudo apt-get install --assume-yes git curl
 
+# install go-task
 curl -fsSL https://raw.githubusercontent.com/taylormonacelli/ringgem/master/install-go-task-on-linux.sh | sudo bash
-curl -Lo ringgem-master.zip https://github.com/taylormonacelli/ringgem/archive/refs/heads/master.zip
-unzip ringgem-master.zip -d .
-ls -la ringgem-master
+
+git submodule update --init --recursive
+
+git submodule status
 
 for i in {1..5}; do
-  sudo task --output=prefixed --dir=ringgem-master --verbose northflier
+    sudo task --output=prefixed --dir=ringgem --verbose northflier
 done
