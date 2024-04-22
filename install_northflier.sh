fe28ff0b0117f38fc2feff96689a46bf435e1f6a
Taylor Monacelli <taylormonacelli@gmail.com>
2024-04-22 15:28:24 -0400

    real men dont make git commit comments

--- install_northflier.sh
+++ install_northflier.sh
@@ -12,6 +12,14 @@ DEBIAN_FRONTEND=noninteractive sudo apt-get install --assume-yes git curl
 # install go-task
 curl -fsSL https://raw.githubusercontent.com/taylormonacelli/ringgem/master/install-go-task-on-linux.sh | sudo bash
 
+grayfiction=$CWD/grayfiction
+ringgem=$grayfiction/ringgem
+git clone --depth 1 https://github.com/taylormonacelli/grayfiction $grayfiction
+
+cd $grayfiction
+git submodule update --init --recursive
+
+cd $ringgem
 for i in {1..5}; do
     sudo task --output=prefixed --dir=$(pwd)/ringgem --verbose northflier
 done
