8618eb5479370e83394474be8a915e00772d56e3
Taylor Monacelli <taylormonacelli@gmail.com>
2024-04-21 23:35:10 -0400

    trigger new build

--- install_northflier.sh
+++ install_northflier.sh
@@ -4,10 +4,9 @@ set -e
 set -x
 set -u
 
-export DEBIAN_FRONTEND=noninteractive
 
 sudo apt-get update
-sudo apt-get install --assume-yes unzip curl
+DEBIAN_FRONTEND=noninteractive sudo apt-get install --assume-yes unzip curl
 
 curl -fsSL https://raw.githubusercontent.com/taylormonacelli/ringgem/master/install-go-task-on-linux.sh | sudo bash
 curl -Lo ringgem-master.zip https://github.com/taylormonacelli/ringgem/archive/refs/heads/master.zip
