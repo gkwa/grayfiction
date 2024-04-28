efc303ac0fc0ea87d2152caaa04fc45c7087265a
Taylor Monacelli <taylormonacelli@gmail.com>
2024-04-28 07:58:14 -0400

    re-add fix for error Could not open file /var/lib/apt/lists/archive.ubuntu.com_ubuntu_dists_focal_multiverse_cnf_Commands-amd64 - open (2: No such file or directory)

--- install_northflier.sh
+++ install_northflier.sh
@@ -6,7 +6,20 @@ set -u
 
 export ORIGCWD=$(pwd)
 
-sudo apt-get update
+# Retry loop for apt update
+max_retries=5
+retry_delay=10
+
+for ((i = 1; i <= max_retries; i++)); do
+    sudo apt-get update && break
+    if [ $i -eq $max_retries ]; then
+        echo "Failed to run apt update after $max_retries attempts. Exiting."
+        exit 1
+    fi
+    echo "apt update failed. Retrying in $retry_delay seconds... (Attempt $i/$max_retries)"
+    sleep $retry_delay
+done
+
 DEBIAN_FRONTEND=noninteractive sudo apt-get install --assume-yes git curl
 
 # install go-task
