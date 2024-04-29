0e2d31acca95951783e012faaedd5002d513ba3b
Taylor Monacelli <taylormonacelli@gmail.com>
2024-04-28 21:02:42 -0400

    move to homebrew

--- install_northflier.sh
+++ install_northflier.sh
@@ -4,21 +4,25 @@ set -e
 set -x
 set -u
 
+apt_update_with_retry() {
+    # Retry loop for apt update
+    max_retries=5
+    retry_delay=10
+
+    for ((i = 1; i <= max_retries; i++)); do
+        sudo apt-get update && break
+        if [ $i -eq $max_retries ]; then
+            echo "Failed to run apt update after $max_retries attempts. Exiting."
+            exit 1
+        fi
+        echo "apt update failed. Retrying in $retry_delay seconds... (Attempt $i/$max_retries)"
+        sleep $retry_delay
+    done
+}
+
 export ORIGCWD=$(pwd)
 
-# Retry loop for apt update
-max_retries=5
-retry_delay=10
-
-for ((i = 1; i <= max_retries; i++)); do
-    sudo apt-get update && break
-    if [ $i -eq $max_retries ]; then
-        echo "Failed to run apt update after $max_retries attempts. Exiting."
-        exit 1
-    fi
-    echo "apt update failed. Retrying in $retry_delay seconds... (Attempt $i/$max_retries)"
-    sleep $retry_delay
-done
+apt_update_with_retry
 
 DEBIAN_FRONTEND=noninteractive sudo apt-get install --assume-yes git curl
 
