088fd962741df7984d1273aa02aa90bb357d1f39
Taylor Monacelli <taylormonacelli@gmail.com>
2024-05-02 18:24:21 -0700

    [skip ci] dumb things down to keep debugging simple

--- install_northflier.sh
+++ install_northflier.sh
@@ -4,8 +4,6 @@ set -e
 set -x
 set -u
 
-export ORIGCWD=$(pwd)
-
 apt_update_with_retry() {
     max_retries=5
     retry_delay=10s
@@ -23,20 +21,20 @@ apt_update_with_retry() {
 
 apt_update_with_retry
 
+mkdir -p /opt/grayfiction
+cd /opt/grayfiction
+
 DEBIAN_FRONTEND=noninteractive sudo apt-get install --assume-yes git curl
 
 # install go-task
 curl -fsSL https://raw.githubusercontent.com/taylormonacelli/ringgem/master/install-go-task-on-linux.sh | sudo bash
 
-gray=$ORIGCWD/grayfiction
-ringgem=$gray/ringgem
-
-git clone --depth 1 https://github.com/taylormonacelli/grayfiction $gray
-cd $gray
+git clone --depth 1 https://github.com/taylormonacelli/grayfiction /opt/grayfiction
+cd /opt/grayfiction
 git submodule update --init --recursive
-cd $ringgem
+cd /opt/grayfiction/ringgem
 
-sudo task --output=prefixed --dir=$ringgem --verbose install-homebrew-on-linux
+sudo task --output=prefixed --dir=/opt/grayfiction/ringgem --verbose install-homebrew-on-linux
 sudo --login --user linuxbrew brew install taylormonacelli/homebrew-tools/howbob
 
 old_xtrace=${-//[^x]/}
@@ -44,7 +42,7 @@ set +x
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 if [[ -n $old_xtrace ]]; then set -x; else set +x; fi
 
-howbob run --path=$ringgem/homebrew.k --brewfile=/tmp/Brewfile --checker=/tmp/versions.sh
+howbob run --path=/opt/grayfiction/ringgem/homebrew.k --brewfile=/tmp/Brewfile --checker=/tmp/versions.sh
 for i in {1..3}; do
     sudo --user linuxbrew --login brew bundle --file=/tmp/Brewfile
 done
@@ -53,7 +51,7 @@ export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
 bash -e /tmp/versions.sh
 
 for i in {1..3}; do
-    sudo task --output=prefixed --dir=$ringgem --verbose northflier
+    sudo task --output=prefixed --dir=/opt/grayfiction/ringgem --verbose northflier
 done
 
 rm -f /tmp/versions.sh
