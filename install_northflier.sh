9b2f48eb53fc4a52807de9eb4dd935f2fe78e070
Taylor Monacelli <taylormonacelli@gmail.com>
2024-05-02 19:05:25 -0700

    workaround permissiond denied

--- install_northflier.sh
+++ install_northflier.sh
@@ -21,21 +21,17 @@ apt_update_with_retry() {
 
 apt_update_with_retry
 
-mkdir -p /opt/grayfiction
-cd /opt/grayfiction
-
-
 DEBIAN_FRONTEND=noninteractive sudo apt-get install --assume-yes git curl
 
 # install go-task
 curl -fsSL https://raw.githubusercontent.com/taylormonacelli/ringgem/master/install-go-task-on-linux.sh | sudo bash
 
-git clone --depth 1 https://github.com/taylormonacelli/grayfiction /opt/grayfiction
-cd /opt/grayfiction
+git clone --depth 1 https://github.com/taylormonacelli/grayfiction
+cd grayfiction
 git submodule update --init --recursive
-cd /opt/grayfiction/ringgem
+cd grayfiction/ringgem
 
-sudo task --output=prefixed --dir=/opt/grayfiction/ringgem --verbose install-homebrew-on-linux
+sudo task --output=prefixed --verbose install-homebrew-on-linux
 sudo --login --user linuxbrew brew install taylormonacelli/homebrew-tools/howbob
 
 old_xtrace=${-//[^x]/}
@@ -43,7 +39,7 @@ set +x
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 if [[ -n $old_xtrace ]]; then set -x; else set +x; fi
 
-howbob run --path=/opt/grayfiction/ringgem/homebrew.k --brewfile=/tmp/Brewfile --checker=/tmp/versions.sh
+howbob run --path=homebrew.k --brewfile=/tmp/Brewfile --checker=/tmp/versions.sh
 for i in {1..3}; do
     sudo --user linuxbrew --login brew bundle --file=/tmp/Brewfile
 done
@@ -52,7 +48,7 @@ export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
 bash -e /tmp/versions.sh
 
 for i in {1..3}; do
-    sudo task --output=prefixed --dir=/opt/grayfiction/ringgem --verbose northflier
+    sudo task --output=prefixed --verbose northflier
 done
 
 rm -f /tmp/versions.sh
